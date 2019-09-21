8337cb571b8efdaf6d2a812ef142d63f9769e318
Jason <jasonnova@protonmail.com>
2019-09-20 22:37:40 -0700

    Erased test junk.

--- test.cpp
+++ test.cpp
@@ -5,15 +5,5 @@
 
 int main(int argc, char const *argv[])
 {
-    int thing = ' ';
-    while(isspace(thing)) {
-        thing = getchar();
-    }
-    /*
-    do
-        thing = getchar();
-    while (thing != EOF && thing != '\n' && thing != '\r');
-    */
-    std::cout << thing << std::endl;
     return 0;
 }
